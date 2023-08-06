a3ed71a2ce765f905f0afd4d71a07acbdb7c87cb
Victor <theonlypwner@users.noreply.github.com>
2023-08-06 17:27:34 -0600

    Remove headers (already covered by LICENSE.txt)

--- ic/__init__.py
+++ ic/__init__.py
@@ -1,19 +1,6 @@
 #!/usr/bin/env python
 # Export all public functions
 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 from .q1 import max_profit
 from .q2 import get_products_of_all_ints_except_at_index
 from .q2 import get_products_of_all_ints_except_at_index2
--- ic/q2.py
+++ ic/q2.py
@@ -1,19 +1,6 @@
 #!/usr/bin/env python
 # Question 2: Product of All Other Numbers (product-of-other-numbers)
 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 
 def get_products_of_all_ints_except_at_index(a):
     """Compute an array with each entry replaced by the product
