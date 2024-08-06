c6b7365deb72cbc97fc362899eec56066ad932af
Victor <theonlypwner@users.noreply.github.com>
2024-08-05 21:31:07 -0600

    q02: use subTest

--- tests/test_q02.py
+++ tests/test_q02.py
@@ -4,6 +4,15 @@ import ic
 import unittest
 
 
+tests = (
+    ('given', [1, 7, 3, 4], [84, 12, 28, 21]),
+    ('example2', [1, 2, 6, 5, 9], [540, 270, 90, 108, 60]),
+    ('example3', [3, 1, 2, 5, 6, 4], [240, 720, 360, 144, 120, 180]), # The solution to this one was not posted in the question
+    ('example4', [2, 4, 10], [40, 20, 8]),
+    ('zero1', [1, 0, 1], [0, 1, 0]),
+    ('zero2', [1, 0, 0], [0, 0, 0]),
+)
+
 class TestQ2(unittest.TestCase):
 
     def check(self, array, result):
@@ -12,21 +21,7 @@ class TestQ2(unittest.TestCase):
         self.assertEqual(
             ic.get_products_of_all_ints_except_at_index2(array), result)
 
-    def test_given(self):
-        self.check([1, 7, 3, 4], [84, 12, 28, 21])
-
-    def test_example2(self):
-        self.check([1, 2, 6, 5, 9], [540, 270, 90, 108, 60])
-
-    def test_example3(self):
-        # The solution to this one was not posted in the question
-        self.check([3, 1, 2, 5, 6, 4], [240, 720, 360, 144, 120, 180])
-
-    def test_example4(self):
-        self.check([2, 4, 10], [40, 20, 8])
-
-    def test_zero1(self):
-        self.check([1, 0, 1], [0, 1, 0])
-
-    def test_zero2(self):
-        self.check([1, 0, 0], [0, 0, 0])
+    def test(self):
+        for name, input, expected in tests:
+            with self.subTest(name=name):
+                self.check(input, expected)
