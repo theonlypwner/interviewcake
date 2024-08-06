ab867fcca0e2041f33b3290695cf082f36d11274
Victor <theonlypwner@users.noreply.github.com>
2024-08-05 21:24:10 -0600

    q01: use subTest

--- tests/test_q01.py
+++ tests/test_q01.py
@@ -4,34 +4,32 @@ import ic
 import unittest
 
 
+tests = (
+    ('standard', [1, 0, 10], [10, [(1, 2)]]),
+    ('negative', [1, -1, 10], [11, [(1, 2)]]),
+    ('no_profit0', [1, -1, -1], [0, [(1, 2)]]),
+    ('no_profit1', [1, 1, -1, -1, -2, -2], [0, [(0, 1), (2, 3), (4, 5)]]),
+    ('forced_loss', [100, 90, 20, 10], [-10, [(0, 1), (2, 3)]]),
+    ('no_shorting', [7, 8, 0], [1, [(0, 1)]]), # can't short at 8 and cover at 0
+    ('multiple', [0, 2, 2, 4, 0, 4], [4, [(0, 3), (0, 5), (4, 5)]]),
+)
+tests_error = (
+    ('empty', []),
+    ('single0', [0]),
+    ('single1', [1]),
+)
+
 class TestQ1(unittest.TestCase):
 
-    def check(self, input, output):
-        output[1] = set(output[1])
-        self.assertEqual(ic.max_profit(input), tuple(output))
+    def test(self):
+        for name, input, expected in tests:
+            with self.subTest(name=name):
+                expected[1] = set(expected[1])
+                actual = ic.max_profit(input)
+                self.assertEqual(actual, tuple(expected))
 
-    def test_empty(self):
+    def test_error_empty(self):
         self.assertRaises(ValueError, lambda: ic.max_profit([]))
 
-    def test_single(self):
+    def test_error_single(self):
         self.assertRaises(ValueError, lambda: ic.max_profit([0]))
-
-    def test_standard(self):
-        self.check([1, 0, 10], [10, [(1, 2)]])
-
-    def test_negative(self):
-        self.check([1, -1, 10], [11, [(1, 2)]])
-
-    def test_no_profit(self):
-        self.check([1, -1, -1], [0, [(1, 2)]])
-        self.check([1, 1, -1, -1, -2, -2], [0, [(0, 1), (2, 3), (4, 5)]])
-
-    def test_forced_loss(self):
-        self.check([100, 90, 20, 10], [-10, [(0, 1), (2, 3)]])
-
-    def test_no_shorting(self):
-        # can't short at 8 and cover at 0
-        self.check([7, 8, 0], [1, [(0, 1)]])
-
-    def test_multiple(self):
-        self.check([0, 2, 2, 4, 0, 4], [4, [(0, 3), (0, 5), (4, 5)]])
