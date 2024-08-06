9fd0283275bbe368afd9f96828e7f7d11cfea59a
Victor <theonlypwner@users.noreply.github.com>
2024-08-05 21:27:21 -0600

    q01: add ic sample code test cases

--- tests/test_q01.py
+++ tests/test_q01.py
@@ -12,6 +12,13 @@ tests = (
     ('forced_loss', [100, 90, 20, 10], [-10, [(0, 1), (2, 3)]]),
     ('no_shorting', [7, 8, 0], [1, [(0, 1)]]), # can't short at 8 and cover at 0
     ('multiple', [0, 2, 2, 4, 0, 4], [4, [(0, 3), (0, 5), (4, 5)]]),
+
+    ('ic_price_goes_up_then_down', [1, 5, 3, 2], [4, [(0, 1)]]),
+    ('ic_price_goes_down_then_up', [7, 2, 8, 9], [7, [(1, 3)]]),
+    ('ic_big_increase_then_small_increase', [2, 10, 1, 4], [8, [(0, 1)]]),
+    ('ic_price_goes_up_all_day', [1, 6, 7, 9], [8, [(0, 3)]]),
+    ('ic_price_goes_down_all_day', [9, 7, 4, 1], [-2, [(0, 1)]]),
+    ('ic_price_stays_the_same_all_day', [1, 1, 1, 1], [0, [(0, 1), (0, 2), (0, 3), (1, 2), (1, 3), (2, 3)]]),
 )
 tests_error = (
     ('empty', []),
