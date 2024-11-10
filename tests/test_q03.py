8e09984ac4454ecd2c6ffae8e38b9d31c5414ef2
Victor <theonlypwner@users.noreply.github.com>
2024-11-10 10:33:31 -0700

    q03: use subTest

--- tests/test_q03.py
+++ tests/test_q03.py
@@ -4,9 +4,47 @@ import ic
 import unittest
 
 
+tests = (
+    ('gotcha', 3, [-10, -10, 1, 3, 2], 300, [-10, -10, 3]),
+    ('example', 3, [1, 10, -5, 1, -100], 5000, [10, -5, -100]),
+    ('zeros', 3, [0, 0, 0, 1, -100], 0),
+    ('zeros_all', 3, [0, 0, 0, 0, 0], 0, [0, 0, 0]),
+    ('negative_odd', 3, [-1, -2, -3, -2, -6, -4], -4, [-1, -2, -2]),
+    ('negative_odd_with_zero', 3, [-1, -2, -3, -2, -6, -4, 0], 0),
+    ('swap_small_pos_big_neg', 3, [-5, 4, 3, -2, 1, 0], 40, [-5, 4, -2]),
+    ('swap_small_neg_big_pos', 3, [5, 4, -3, 2, -1, 0], 40, [5, 4, 2]),
+    ('swap_no_neg_outside', 3, [-5, 4, 3, 2, 1], 24, [2, 4, 3]),
+    ('swap_no_pos_outside', 3, [5, 4, -3, -2, -1], 30, [5, -2, -3]),
+    ('swap_no_pos_inside', 3, [-5, -4, -3, 2, 1], 40, [-5, -4, 2]),
+    ('single', 1, [-1], -1, [-1]),
+    ('all', 5, [-1, 1, 2, -2, 3], 12, [-1, 1, 2, -2, 3]),
+    ('multiple_solutions0', 3, [2, 2, -2, 1, -1, 0], 4, [[2, 2, 1], [2, -2, -1]]),
+    ('multiple_solutions1', 3, [2, 2, 2, -2, -2, 0], 8, ((2, 2, 2), (2, -2, -2))),
+    # from https://stackoverflow.com/a/42153733/548194
+    ('so_a0', 4, [1, 2, 3, 4, 5, 6, 7, 8], 1680, [5, 6, 7, 8]),
+    ('so_a1', 4, [1, -2, 3, 4, 5, 100, 2, 3, 1], 6000, [3, 4, 5, 100]),
+    ('so_a2', 4, [-10, -10, 1, 3, 2], 600, [2, 3, -10, -10]),
+    ('so_a3', 4, [1000, 7, -6, 2, 2], 28000, [1000, 7, 2, 2]),
+    ('so_a4', 2, [-1, 0, 1], 0),
+    ('so_a5', 4, [2, 5, 8, 9, 1, 3, 7], 2520, [5, 7, 8, 9]),
+    ('so_a6', 2, [-1, -1, 2, 1], 2, [2, 1]),
+    ('so_a7', 2, [-1000, -1, 2, 3], 1000, [-1, -1000]),
+    ('so_a8', 2, [3, 5, 2, 8, 3], 40, [5, 8]),
+    ('so_a9', 2, [-1000, -1, 2, 3, 4, 5, 6, 7], 1000, [-1, -1000]),
+    # from https://stackoverflow.com/q/22104635/548194
+    ('so_b0', 3, [4, 1, -7, -8, 9], 504, [-7, -8, 9]),
+    ('so_b1', 3, [1, 3, -6, 3, 5], 45, [3, 3, 5]),
+)
+
+tests_error = (
+    ('k_zero', [1], 0, ValueError, 'K must be at least 1'),
+    ('k_big_1_2', [1], 2, ValueError, 'List does not contain at least K elements'),
+    ('k_big_1_3', [1], 3, ValueError, 'List does not contain at least K elements'),
+)
+
 class TestQ3(unittest.TestCase):
 
-    def check(self, array, expected, expected_list=None, K=3):
+    def check(self, K, array, expected, expected_list=None):
         actual = ic.highest_product(array, K)
         self.assertEqual(actual, expected)
 
@@ -30,67 +68,12 @@ class TestQ3(unittest.TestCase):
 
         self.assertIn(exception_message, context.exception.args[0])
 
-    def test_gotcha(self):
-        self.check([-10, -10, 1, 3, 2], 300, [-10, -10, 3])
+    def test(self):
+        for test in tests:
+            with self.subTest(name=test[0]):
+                self.check(*test[1:])
 
-    def test_example(self):
-        self.check([1, 10, -5, 1, -100], 5000, [10, -5, -100])
-
-    def test_zeros(self):
-        self.check([0, 0, 0, 1, -100], 0)
-
-    def test_zeros_all(self):
-        self.check([0, 0, 0, 0, 0], 0, [0, 0, 0])
-
-    def test_negative_odd(self):
-        self.check([-1, -2, -3, -2, -6, -4], -4, [-1, -2, -2])
-
-    def test_negative_odd_with_zero(self):
-        self.check([-1, -2, -3, -2, -6, -4, 0], 0)
-
-    def test_swap_small_pos_big_neg(self):
-        self.check([-5, 4, 3, -2, 1, 0], 40, [-5, 4, -2])
-
-    def test_swap_small_neg_big_pos(self):
-        self.check([5, 4, -3, 2, -1, 0], 40, [5, 4, 2])
-
-    def test_swap_no_neg_outside(self):
-        self.check([-5, 4, 3, 2, 1], 24, [2, 4, 3])
-
-    def test_swap_no_pos_outside(self):
-        self.check([5, 4, -3, -2, -1], 30, [5, -2, -3])
-
-    def test_swap_no_pos_inside(self):
-        self.check([-5, -4, -3, 2, 1], 40, [-5, -4, 2])
-
-    def test_single(self):
-        self.check([-1], -1, [-1], K=1)
-
-    def test_all(self):
-        self.check([-1, 1, 2, -2, 3], 12, [-1, 1, 2, -2, 3], K=5)
-
-    def test_multiple_solutions(self):
-        self.check([2, 2, -2, 1, -1, 0], 4, [[2, 2, 1], [2, -2, -1]])
-        self.check([2, 2, 2, -2, -2, 0], 8, ((2, 2, 2), (2, -2, -2)))
-
-    def test_k_zero(self):
-        self.check_fail([1], 0, ValueError, 'K must be at least 1')
-
-    def test_k_big(self):
-        self.check_fail([1], 2, ValueError, 'List does not contain at least K elements')
-
-    def test_extra(self):
-        # from https://stackoverflow.com/a/42153733/548194
-        self.check([1, 2, 3, 4, 5, 6, 7, 8], 1680, [5, 6, 7, 8], K=4)
-        self.check([1, -2, 3, 4, 5, 100, 2, 3, 1], 6000, [3, 4, 5, 100], K=4)
-        self.check([-10, -10, 1, 3, 2], 600, [2, 3, -10, -10], K=4)
-        self.check([1000, 7, -6, 2, 2], 28000, [1000, 7, 2, 2], K=4)
-        self.check([-1, 0, 1], 0, K=2)
-        self.check([2, 5, 8, 9, 1, 3, 7], 2520, [5, 7, 8, 9], K=4)
-        self.check([-1, -1, 2, 1], 2, [2, 1], K=2)
-        self.check([-1000, -1, 2, 3], 1000, [-1, -1000], K=2)
-        self.check([3, 5, 2, 8, 3], 40, [5, 8], K=2)
-        self.check([-1000, -1, 2, 3, 4, 5, 6, 7], 1000, [-1, -1000], K=2)
-        # from https://stackoverflow.com/q/22104635/548194
-        self.check([4, 1, -7, -8, 9], 504, [-7, -8, 9])
-        self.check([1, 3, -6, 3, 5], 45, [3, 3, 5])
+    def test_error(self):
+        for test in tests_error:
+            with self.subTest(name=test[0]):
+                self.check_fail(*test[1:])
