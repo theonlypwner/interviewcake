140b988551ff97326d8971fe5077bb2c833e91f9
Victor <theonlypwner@users.noreply.github.com>
2024-11-10 10:09:33 -0700

    q04: use subTest

--- tests/test_q04.py
+++ tests/test_q04.py
@@ -4,6 +4,16 @@ import ic
 import unittest
 
 
+tests = (
+    ('example', [(0, 1), (3, 5), (4, 8), (10, 12), (9, 10)], [(0, 1), (3, 8), (9, 12)]),
+    ('unordered', [(10, 12), (0, 1), (3, 5), (4, 8), (9, 10)], [(0, 1), (3, 8), (9, 12)]),
+    ('gotcha_merge', [(1, 2), (2, 3)], [(1, 3)]),
+    ('gotcha_subsume', [(1, 5), (2, 3)], [(1, 5)]),
+    ('gotcha_multimerge', [(1, 10), (2, 6), (3, 5), (7, 9)], [(1, 10)]),
+    ('large', [(1000000000, 2000000000), (1900000000, 3000000000)], [(1000000000, 3000000000)]),
+    ('empty', [], []),
+)
+
 class TestQ4(unittest.TestCase):
 
     def check(self, input, output):
@@ -13,23 +23,7 @@ class TestQ4(unittest.TestCase):
         if diff <= 2 ** 20:
             self.assertEqual(ic.union_ranges2(input), output)
 
-    def test_example(self):
-        self.check([(0, 1), (3, 5), (4, 8), (10, 12), (9, 10)], [(0, 1), (3, 8), (9, 12)])
-
-    def test_unordered(self):
-        self.check([(10, 12), (0, 1), (3, 5), (4, 8), (9, 10)], [(0, 1), (3, 8), (9, 12)])
-
-    def test_gotcha_merge(self):
-        self.check([(1, 2), (2, 3)], [(1, 3)])
-
-    def test_gotcha_subsume(self):
-        self.check([(1, 5), (2, 3)], [(1, 5)])
-
-    def test_gotcha_multimerge(self):
-        self.check([(1, 10), (2, 6), (3, 5), (7, 9)], [(1, 10)])
-
-    def test_large(self):
-        self.check([(1000000000, 2000000000), (1900000000, 3000000000)], [(1000000000, 3000000000)])
-
-    def test_empty(self):
-        self.check([], [])
+    def test(self):
+        for name, input, expected in tests:
+            with self.subTest(name=name):
+                self.check(input, expected)
